3f5427b26879beb91907a0d3decb8aca53d498c3
Muhammad Jahran <rann020070.com>
2024-05-15 14:58:48 +0700

    update

--- app/Http/Controllers/FrontEnd/FrontEndController.php
+++ app/Http/Controllers/FrontEnd/FrontEndController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers\FrontEnd;
 
-use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Product;
 
 class FrontEndController extends Controller
 {
-    public function index ()
+    public function index()
     {
-        return view('pages.Frontend.parent');
+        $category = Category::select('id', 'name')->latest()->get();
+        $product = Product::with('product_galleries')->select('id', 'name', 'slug', 'price')->latest()->get();
+
+        return view('pages.Frontend.index', compact(
+            'category',
+            'product'
+        ));
     }
 }
